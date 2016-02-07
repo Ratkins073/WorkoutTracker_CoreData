4b6e8e0051c8d4c152f1fd66aec16dd2cce7cd20
Ryan Atkins <ryanatkins@ryans-macbook-air.local>
2016-02-07 14:37:12 -0600

    Initial Commit

---
+++ WorkoutTracker_CoreDataTests/WorkoutTracker_CoreDataTests.swift
@@ -0,0 +1,36 @@
+//
+//  WorkoutTracker_CoreDataTests.swift
+//  WorkoutTracker_CoreDataTests
+//
+//  Created by Ryan Atkins on 2/7/16.
+//  Copyright © 2016 Ratkins Creations. All rights reserved.
+//
+
+import XCTest
+@testable import WorkoutTracker_CoreData
+
+class WorkoutTracker_CoreDataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
